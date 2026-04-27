6d90614cb345d4d51d7595efd4ad05d432834f2b
Alan <alankemalov@gmail.com>
2026-04-27 13:11:29 +0300

    add basic express server

---
+++ hotel-app/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hotel database management API is running",
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
